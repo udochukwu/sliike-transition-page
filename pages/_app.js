1f3aca665d6d386bb3b0914f962c5dc664cb9349
udochukwu <nnaji_udochukwu@yahoo.com>
2022-02-21 13:34:06 +0100

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { appWithTranslation } from 'next-i18next';
 import '../styles/globals.scss';
 import '../styles/Home.scss';
+import '../assets/fonts/fonts.css';
 import '../next-i18next.config';
 function MyApp({ Component, pageProps }) {
   const client = new ApolloClient({
